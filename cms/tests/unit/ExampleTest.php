dac09ac7395e50a0588d5a2b60807918bac3cb81
Mike-Tranzit <mihail.devyatov@port-tranzit.ru>
2018-11-06 18:30:59 +0300

    (test): init codeception.yml

---
+++ cms/tests/unit/ExampleTest.php
@@ -0,0 +1,25 @@
+<?php
+
+use \cms\Example;
+
+class ExampleTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testSomeFeature()
+    {
+
+    }
+}
