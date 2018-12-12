b57e2be5790242ade141279aedd3c778717ddb28
Mike-Tranzit <mihail.devyatov@port-tranzit.ru>
2018-12-12 09:32:06 +0300

    tests(InformationTest): cat methods to advance common methods

--- cms/modules/v1/models/Information.php
+++ cms/modules/v1/models/Information.php
@@ -12,13 +12,13 @@ use yii\web\HttpException;
 
 interface ITInformation
 {
-    function getUserInformation();
+    public function getUserInformation();
 
-    function getInvoices();
+    public function getInvoices();
 
-    function concatDataAndReturn();
+    public function concatDataAndReturn();
 
-    function getActiveRequest();
+    public function getActiveRequest();
 }
 
 class Information implements ITInformation
@@ -34,7 +34,7 @@ class Information implements ITInformation
      *
      * @param  mixed $login
      * @codeCoverageIgnore
-	 * @ignore Codeception specific
+     * @ignore Codeception specific
      * @return void
      */
     public function __construct($login)
@@ -47,14 +47,16 @@ class Information implements ITInformation
         return $this->information;
     }
 
-    function getActiveRequest()
+    public function getActiveRequest()
     {
         $active = SubscriptionsActive::find()->where(['user_id' => $this->information['user']['id']])->one();
-        if(!$active) {
+        if (!$active) {
             $new_subscription_active = new SubscriptionsActive(['user_id'=>$this->information['user']['id'], 'requests_left'=>0]);
-            if($new_subscription_active->save()) $active = $new_subscription_active;
+            if ($new_subscription_active->save()) {
+                $active = $new_subscription_active;
+            }
         }
-        if($active) {
+        if ($active) {
             $active = ArrayHelper::toArray($active);
             $this->information['user']['subscriptions_active'] = ['requests_left' => $active['requests_left'], 'id' => $active['id']];
         }
@@ -63,14 +65,25 @@ class Information implements ITInformation
     public function getUserInformation()
     {
         $userRecord = \cms\modules\v1\models\base\Users::find()->where(['login'=> trim($this->login)])->one();
-        if (!$userRecord) throw new HttpException(404, 'Ошибка. Пользовать не найден');
+        if (!$userRecord) {
+            throw new HttpException(404, 'Ошибка. Пользовать не найден');
+        }
         $userRecord = ArrayHelper::toArray($userRecord);
-        $this->information['user'] = ['id' => $userRecord['id'], 'company'=>$userRecord['company'], 'password'=>'', 'name' => $userRecord['name'], 'status_id' => $this->statusExpiry[(int)$userRecord['status_id']], 'status_expiry' => $userRecord['status_expiry'], 'confirm' => $this->confirm[$userRecord['confirm']]];
+        $this->information['user'] = [
+            'id' => $userRecord['id'], 
+            'company'=>$userRecord['company'], 
+            'password'=>'', 
+            'name' => $userRecord['name'], 
+            'status_id' => $this->statusExpiry[(int)$userRecord['status_id']], 
+            'status_expiry' => $userRecord['status_expiry'], 
+            'confirm' => $this->confirm[$userRecord['confirm']]
+        ];
     }
 
-    public function mobileRegistration(){
+    public function mobileRegistration()
+    {
         $mobileRegistration = MobileRegistration::find()->where(['user_id' => $this->information['user']['id']])->andWhere(['<', 'last_sms_date', 'now() - interval 1 day'])->orderBy(['id' => SORT_DESC])->one();
-        if($mobileRegistration){
+        if ($mobileRegistration) {
             $this->information['user']['activation_code'] = $mobileRegistration->activation_code;
             $this->information['user']['last_sms_date'] = $mobileRegistration->last_sms_date;
         }
@@ -81,6 +94,8 @@ class Information implements ITInformation
         $invoices = Invoices::find()->where(['userIdCreate' => $this->information['user']['id']])->andWhere(['<', 'datecreate', 'now() - interval 10 day'])->orderBy(['id' => SORT_DESC])->limit(10)->all();
         if ($invoices) {
             $this->information['invoices'] = ArrayHelper::toArray($invoices);
-        }else $this->information['invoices'] = [];
+        } else {
+            $this->information['invoices'] = [];
+        }
     }
 }
--- cms/tests/unit/models/InformationTest.php
+++ cms/tests/unit/models/InformationTest.php
@@ -2,24 +2,36 @@
 namespace cms\tests\models;
 
 use Yii;
-use cms\modules\v1\models\base\{Invoices, MobileRegistration, SubscriptionsActive};
+use cms\modules\v1\models\base\Invoices;
+use cms\modules\v1\models\base\MobileRegistration;
+use cms\modules\v1\models\base\SubscriptionsActive;
 use cms\modules\v1\models\Information;
 
 use yii\web\HttpException;
 
+
 use common\fixtures\UsersFixture;
 use Codeception\Stub;
 use Codeception\Specify;
+use Codeception\Exception\Fail;
+use Codeception\Util\HttpCode;
+
 class InformationTest extends \Codeception\Test\Unit
 {
     /**
      * @var \cms\tests\UnitTester
      */
 
+    /**
+     * TODO Вынести в отдельный метод запрос приватных свойств
+     */
+    
     use Specify;
     protected $tester;
     protected $model;
     private $_fixture;
+    const FAKE_ID = 0;
+    const COUNT_USER_INFORMATION_ARRAY = 7;
 
     protected function _before()
     {
@@ -55,7 +67,7 @@ class InformationTest extends \Codeception\Test\Unit
      */
     public function testHasPrivateDefaultPropertys()
     {
-        $confirm = $this->tester->getPrivateProperty( Information::class, 'confirm' );
+        $confirm = $this->tester->getPrivateProperty(Information::class, 'confirm');
         expect($this->model->concatDataAndReturn())->isEmpty();
         expect($confirm->getValue($this->model))->count(2);
     }
@@ -65,24 +77,30 @@ class InformationTest extends \Codeception\Test\Unit
      */
     public function testGetActiveRequestExist()
     {
-        $this->specify('Check that record exist', function() {
-
-            $property = $this->tester->getPrivateProperty( 
-                Information::class, 
-                'information' 
-            );
-
-            $property->setValue($this->model, [
-                'user' => [
-                    'id' => 10129
-                ]
-            ]);
-
-            $this->model->getActiveRequest();
-
-            $information = $property->getValue( $this->model );
+        $this->specify('Check that record exist', function () {
+            $information = $this->loadInformationPropertyUserId(10129);
 
             expect('Information array has key: subscriptions_active', $information['user'])->hasKey('subscriptions_active');
+        });
+    }
+
+    /**
+     * @group active
+     */
+    public function testGetActiveRequestNotExist()
+    {
+        $this->specify('Check that record will be a new', function () {
+            $information = $this->loadInformationPropertyUserId(self::FAKE_ID);
+
+            $this->tester->seeRecord(SubscriptionsActive::class, [
+                'user_id' => 0
+            ]);
+
+            expect('Information array has key: subscriptions_active', $information['user'])->hasKey('subscriptions_active');
+
+            expect('Default left request is 0', $information['user']['subscriptions_active']['requests_left'])->equals(0);
+
+            SubscriptionsActive::deleteAll(['user_id' => self::FAKE_ID]);
         });
     }
 
@@ -90,6 +108,131 @@ class InformationTest extends \Codeception\Test\Unit
     public function testLoginFilter()
     {
         expect($this->model->login)->equals('+79184868904');
+    }
 
+    /**
+     * Тест на отсутствие пользователя в базе
+     *
+     * @group user
+     * @return void
+     */
+    public function testUserNotFound()
+    {
+        $this->specify('User Not found', function () {
+            $this->model->login = "70000000000";
+
+            try {
+                $this->model->getUserInformation();
+                $this->fail('Exception is not call');
+            } catch (HttpException $e) {
+                expect('Check Exception code', $e->statusCode)->equals(HttpCode::NOT_FOUND);
+                expect("Check Exception message", $e->getMessage())->equals('Ошибка. Пользовать не найден');
+            }
+        });
+    }
+
+    /**
+     * Тест на существование пользователя
+     *
+     * @group user
+     * @return void
+     */
+    public function testUserExist()
+    {
+        $this->specify('User if found', function () {
+            $this->model->login = '+79184868905';
+
+            $this->model->getUserInformation();
+
+            $property = $this->tester->getPrivateProperty(
+                Information::class,
+                'information'
+            );
+
+            $information = $property->getValue($this->model);
+
+            expect("User information is array", is_array($information['user']))->true();
+
+            expect("Check count user array", count($information['user']) == self::COUNT_USER_INFORMATION_ARRAY)->true();
+        });
+    }
+
+
+    /**
+     * Тестирование отсутствия платежей пользователя
+     *
+     * @return void
+     */
+    public function testInvoicesIsEmpty()
+    {
+        $property = $this->getInformation(self::FAKE_ID);
+        
+        $information = $this->getValuePrivateProperty($property);
+
+        $this->model->getInvoices();
+
+        expect('Invoices array is empty', $information['invoices'])->isEmpty();
+    }
+
+
+    
+    //
+    // ────────────────────────────────────────────────────────────────────── I ──────────
+    //   :::::: A D V A N C E   M E T H O D S : :  :   :    :     :        :          :
+    // ────────────────────────────────────────────────────────────────────────────────
+    //
+
+    /**
+     * Заполнение приватного свойства Information -> user -> id
+     *
+     * @param  integer $id
+     *
+     * @return array
+     */
+    public function loadInformationPropertyUserId(int $id)
+    {
+        $property = $this->getInformation($id);
+
+        $this->model->getActiveRequest();
+
+        $information = $this->getValuePrivateProperty($property);
+
+        return $information;
+    }
+
+    /**
+     * getInformation
+     *
+     * @param  integer $id
+     *
+     * @return void
+     */
+    public function getInformation(int $id)
+    {
+        $property = $this->tester->getPrivateProperty(
+            Information::class,
+            'information'
+        );
+
+        $property->setValue($this->model, [
+            'user' => [
+                'id' => $id
+            ],
+            'invoices' => []
+        ]);
+
+        return $property;
+    }
+
+    /**
+     * getValuePrivateProperty
+     *
+     * @param  object $property
+     *
+     * @return object
+     */
+    public function getValuePrivateProperty($property)
+    {
+        return $property->getValue($this->model);
     }
 }
