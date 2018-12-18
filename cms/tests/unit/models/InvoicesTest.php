256b7412d4be5777ec2ab678aa300fb26915bf94
Mike-Tranzit <mihail.devyatov@port-tranzit.ru>
2018-12-18 15:03:56 +0300

    test(Invoicestest): init

---
+++ cms/tests/unit/models/InvoicesTest.php
@@ -0,0 +1,23 @@
+<?php namespace cms\tests\models;
+
+class InvoicesTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \cms\tests\UnitTester
+     */
+    protected $tester;
+    
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testSomeFeature()
+    {
+
+    }
+}
