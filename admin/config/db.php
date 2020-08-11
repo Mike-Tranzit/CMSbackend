f63588f4b32de5838e6f2a4ec1feff56ad1c6615
Mike-Tranzit <mike.anapa@gmail.com>
2020-08-11 14:47:59 +0300

    update: db.php

--- admin/config/db.php
+++ admin/config/db.php
@@ -1,14 +1,14 @@
 <?php
 
 return [
-       'class' => 'yii\db\Connection',
+    /*   'class' => 'yii\db\Connection',
         'dsn' => 'mysql:host=localhost;dbname=nztmodule3',
         'username' => 'root',
         'password' => '',
-        'charset' => 'utf8',
-/*	'class' => 'yii\db\Connection',
+        'charset' => 'utf8',*/
+	'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=192.168.2.104;dbname=nztmodule3',
     'username' => 'all',
     'password' => '1111',
-    'charset' => 'utf8',*/
+    'charset' => 'utf8',
 ];
--- cms/config/db.php
+++ cms/config/db.php
@@ -2,8 +2,8 @@
 
 return [
         'class' => 'yii\db\Connection',
-        'dsn' => 'mysql:host=localhost;dbname=glonass',
-        'username' => 'root',
-        'password' => '',
+        'dsn' => 'mysql:host=192.168.2.104;dbname=glonass',
+        'username' => 'all',
+        'password' => '1111',
         'charset' => 'utf8'
 ];
